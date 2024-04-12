598a1c452c0b1f78481057a716808cffd4973c6d
beomsic <qjatjr29@khu.ac.kr>
2024-04-12 15:43:41 +0900

    :tada: begin: 프로젝트 시작

---
+++ src/main/java/beomsic/springsecurityauth/SpringSecurityAuthApplication.java
@@ -0,0 +1,13 @@
+package beomsic.springsecurityauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthApplication.class, args);
+	}
+
+}
